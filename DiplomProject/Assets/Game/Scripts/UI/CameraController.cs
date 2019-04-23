88d45c52888a6dbb45b26b03792922c0319f9f7c
SadOne <rankedme@gmail.com>
2019-04-23 23:42:44 +1000

    SetupCameraController

---
+++ DiplomProject/Assets/Game/Scripts/UI/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform Target;
+	public Camera Camera;
+
+    void Update()
+    {
+		Camera.transform.position = new Vector3(Target.position.x, Target.position.y, -10);
+    }
+}
