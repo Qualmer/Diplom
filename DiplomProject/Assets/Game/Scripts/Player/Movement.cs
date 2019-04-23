5394b2bfd619f4c8960d05eb129bd504a5685fef
SadOne <rankedme@gmail.com>
2019-04-23 23:28:08 +1000

    Starting project

---
+++ DiplomProject/Assets/Game/Scripts/Player/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public float Speed;
+
+	private Rigidbody2D rb;
+	private Vector2 velocity;
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		velocity = input.normalized * Speed;
+		rb.MovePosition(rb.position + velocity * Time.deltaTime);
+    }
+}
