921badfbad27bd7f12774d2eb442561df5a90eca
aQCy <51476122+aQCy@users.noreply.github.com>
2019-06-07 20:34:07 +1000

    1
    1

---
+++ DiplomProject/Assets/Game/Scripts/AI/Creeps/Creep1Stats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creep1Stats : MonoBehaviour
+{
+    public float HP = 100;
+
+    public void AddDamage(float damage)
+    {
+        HP += damage;
+
+        if (HP <= 0)
+        {
+            HP = 0;
+
+            Destroy(gameObject); // удаляем объект
+        }
+
+        // Use this for initialization
+    }
+}
---
+++ DiplomProject/Assets/Game/Scripts/Mechanics/Combat/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    GameObject xyz_range;
+    public int damage ;
+    public int wpn_damage;
+    public int armor;
+    // Use this for initialization
+    public GameObject hpbarTriger;
+    public GameObject plr;
+
+    private void OnTriggerStay(Collider col)
+    {
+
+        switch (col.tag)
+        {
+            case "Enemy_xyz":
+                break;
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+
+		
+	}
+}
